194b46c8220bea6b4b5731c6b42b8ba7c8e34f48
yangtaewoong <tom0323@naver.com>
2024-11-11 12:21:55 +0900

    asdf

---
+++ bird.py
@@ -0,0 +1,92 @@
+from pico2d import get_time, load_image, SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT, load_font
+from state_machine import *
+import game_world
+import game_framework
+
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 10.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 5
+
+class Idle:
+    @staticmethod
+    def enter(bird, e):
+        if start_event(e):
+            bird.action = 0
+            bird.face_dir = 1
+        elif right_down(e) or left_up(e):
+            bird.action = 2
+            bird.face_dir = -1
+        elif left_down(e) or right_up(e):
+            bird.action = 2
+            bird.face_dir = 1
+
+        bird.frame = 0
+        bird.wait_time = get_time()
+
+    @staticmethod
+    def exit(bird, e):
+        pass
+
+    @staticmethod
+    def do(bird):
+        bird.frame = (bird.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
+
+    @staticmethod
+    def draw(bird):
+        bird.image.clip_draw(int(bird.frame) * 180, bird.action * 160, 180, 160, bird.x, bird.y)
+
+class Run:
+    @staticmethod
+    def enter(bird, e):
+        if right_down(e) or left_up(e): # 오른쪽으로 RUN
+            bird.dir, bird.face_dir, bird.action = 1, 1, 1
+        elif left_down(e) or right_up(e): # 왼쪽으로 RUN
+            bird.dir, bird.face_dir, bird.action = -1, -1, 0
+
+    @staticmethod
+    def exit(bird, e):
+        pass
+
+
+    @staticmethod
+    def do(bird):
+        bird.frame = (bird.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
+        bird.x += bird.dir * RUN_SPEED_PPS * game_framework.frame_time
+
+
+    @staticmethod
+    def draw(bird):
+        bird.image.clip_draw(int(bird.frame) * 180, bird.action * 160, 180, 160, bird.x, bird.y)
+
+
+class Bird:
+    def __init__(self):
+        self.x, self.y = 400, 400
+        self.face_dir = 1
+        self.image = load_image('bird_animation.png')
+        self.state_machine = StateMachine(self)
+        self.state_machine.start(Idle)
+        #self.font = load_font('ENCR10B.TTF', 16)
+        self.state_machine.set_transitions(
+            {
+                Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, space_down: Idle},
+                Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, space_down: Run},
+            }
+        )
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        # 여기서 받을 수 있는 것만 걸러야 함. right left  등등..
+        self.state_machine.add_event(('INPUT', event))
+        pass
+
+    def draw(self):
+        self.state_machine.draw()
+        #self.font.draw(self.x - 60, self.y + 50, f'(Time: {get_time(): .2f})', (255,255,0))
---
+++ play_mode.py
@@ -0,0 +1,56 @@
+from pico2d import *
+import game_framework
+
+import game_world
+from grass import Grass
+from boy import Boy
+from bird import Bird
+
+# boy = None
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            bird.handle_event(event)
+
+def init():
+    global grass
+    #global boy
+    global bird
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+
+    #boy = Boy()
+    #game_world.add_object(boy, 1)
+
+    bird =Bird()
+    game_world.add_object(bird, 1)
+
+
+def finish():
+    game_world.clear()
+    pass
+
+
+def update():
+    game_world.update()
+    # 성능 후진 PC 모사
+    #delay(0.1)
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
+
